136eee6a958e760c81b58df572cb491b0570189f
Tabor Wondimu <csmajortabor@gmail.com>
2022-04-03 14:24:12 -0700

    Finished JavaScript Exercise

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Tabor";
+let lastName = "Angel";
+var age = 26;
+let city = "Austin";
+let favoritePassTime = "Coding";
 
 // Exercise 2
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
 // Exercise 3
+let myStory = `I am ${fullName}, I live in ${city}, I enjoy ${favoritePassTime}!`;
+console.log(myStory);
